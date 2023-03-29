94af95bb6a6b439e8efca55ccc70bef9f5e2f2ff
yildiztugba <okra.yildiz@gmail.com>
2023-03-29 13:43:42 +0200

    icons & resume download link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from "react"
 import {
-  AiFillTwitterCircle,
+  AiFillMail,
   AiFillLinkedin,
-  AiFillYoutube 
+  AiFillGithub
 } from "react-icons/ai";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <nav className="py-10 mb-12 flex justify-between">
           <ul className="flex ml-auto pr-32 cursor-pointer">
             <li><a className="bg-gradient-to-r from-purple-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"> Portfolio</a></li>
-            <li><a className="bg-gradient-to-r from-purple-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" >Resume</a></li>
+            <li><a href="https://github.com/yildiztugba/yildiztugba.github.io/raw/uploadpdf/resume-tugba-yildiz.pdf" target="_blank"  className="bg-gradient-to-r from-purple-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" >Resume</a></li>
           </ul>
           </nav>
         </section>
@@ -25,9 +25,13 @@ function App() {
           <div className="text-center p-10" >
             <h1 className="text-5xl py-2 text-teal-600 font-medium">Tugba Yildiz</h1>
             <h3 className="text-2xl py-2" >Front-end Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 max-w-lg mx-auto">Computer Teaching graduate,passionate about front end development. worked as a tutor and IT coordinator. Experienced in a range of modern technologies including JavaScript, React and HTML5/CSS. Problem-solver, hard-worker and organized person.
-</p>
+            <p className="text-md py-5 leading-8 text-gray-800 max-w-lg mx-auto">Computer Teaching graduate,passionate about front end development. worked as a tutor and IT coordinator. Experienced in a range of modern technologies including JavaScript, React and HTML5/CSS. Problem-solver, hard-worker and organized person.</p>
           </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 ">
+          <AiFillMail />
+          <AiFillLinkedin />
+          <AiFillGithub />
+        </div>
         </section>
       </main>
     </div>
