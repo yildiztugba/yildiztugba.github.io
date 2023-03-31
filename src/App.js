73c43fc5bc4fb709617d19a1fca0f4be5acee8b7
yildiztugba <okra.yildiz@gmail.com>
2023-03-31 10:21:35 +0200

    about component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import About from "./components/About"
 import React from "react"
-import {
-  AiFillMail,
-  AiFillLinkedin,
-  AiFillGithub
-} from "react-icons/ai";
+
 
 function App() {
   return (
     <div>
       <main>
-        {/*NAVBAR */} 
         <Navbar />
         
         {/*ABOUT */} 
-        <section>
-          <div className="text-center p-10" >
-            <h1 className="text-5xl py-2 text-teal-600 font-medium">Tugba Yildiz</h1>
-            <h3 className="text-2xl py-2" >Front-end Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 max-w-lg mx-auto">Computer Teaching graduate,passionate about front end development. worked as a tutor and IT coordinator. Experienced in a range of modern technologies including JavaScript, React and HTML5/CSS. Problem-solver, hard-worker and organized person.</p>
-          </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 ">
-          <a href="mailto:okra.yildiz@gmail.com" target="_blank" ><AiFillMail /></a> 
-          <a href="https://www.linkedin.com/in/yildiztugba/" target="_blank" ><AiFillLinkedin /></a>
-          <a href="https://github.com/yildiztugba"target="_blank" ><AiFillGithub /></a>
-        </div>
-        </section>
+        <About />
 
 
 
