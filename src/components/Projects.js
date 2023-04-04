9992e79d970a97daee417cf24aef39f258fe871b
yildiztugba <okra.yildiz@gmail.com>
2023-04-04 13:14:43 +0200

    project5

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ const project1 =require('./project1.png')
 const project2 =require('./project2.png')
 const project3 =require('./project3.png')
 const project4 =require('./project4.png')
+const project5 =require('./project5.png')
 export default function Projects(){
     return(
         <div id="projects" className="w-full p-16"> 
@@ -44,11 +45,11 @@ export default function Projects(){
                     </div>
                 </div>
                 <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-                    <img className="rounded-xl group-hover:opacity-10" src={project1}/>
+                    <img className="rounded-xl group-hover:opacity-10" src={project5}/>
                     <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                        <h3 className="text-2xl text-white tracking-wider text-center">Game:Tenzies</h3>
+                        <h3 className="text-2xl text-white tracking-wider text-center">Travel Project</h3>
                         <p className="pb-4 pt-2 text-white text-center">React Js</p>
-                        <a href="/"><p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer"> More Info</p></a>
+                        <a href="https://yildiztugba.github.io/Travel-Project-with-React/"><p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer"> More Info</p></a>
                     </div>
                 </div>
                 <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
