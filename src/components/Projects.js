6065caf148003e49dca41d4db8577322d5978184
yildiztugba <okra.yildiz@gmail.com>
2023-06-16 15:10:12 +0200

    titles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,115 +10,119 @@ const project8 =require('./project8.png')
 const project9 =require('./project9.png')
 export default function Projects(){
     return(
-       <div class="flex flex-wrap justify-center mx-2">
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project1} alt="Project 1"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 1</h2>
-        <p class="mb-4">Project 1 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
+<div>
+         <p className="tracking-widest uppercase text-5xl py-2 text-center text-teal-600 font-medium"> Projects</p>
+    <h2 className="py-4 text-lg uppercase text-center font-bold" >ADVANCED REACT PROJECTS</h2>
+   
+<div class="flex flex-wrap justify-center mx-2">         
+    <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+        <div class="bg-white shadow-lg rounded-lg">
+        <img class="w-full h-auto" src={project1} alt="Project 1"/>
+        <div class="p-4">
+            <h2 class="text-lg font-bold mb-2">Project 1</h2>
+            <p class="mb-4">Project 1 description</p>
+            <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+            <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+        </div>
+        </div>
     </div>
-  </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 
-  <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
-    <div class="bg-white shadow-lg rounded-lg">
-      <img class="w-full h-auto" src={project2} alt="Project 2"/>
-      <div class="p-4">
-        <h2 class="text-lg font-bold mb-2">Project 2</h2>
-        <p class="mb-4">Project 2 description</p>
-        <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
-        <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
-      </div>
-    </div>
-  </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mt-8">
+            <div class="bg-white shadow-lg rounded-lg">
+            <img class="w-full h-auto" src={project2} alt="Project 2"/>
+            <div class="p-4">
+                <h2 class="text-lg font-bold mb-2">Project 2</h2>
+                <p class="mb-4">Project 2 description</p>
+                <a class="text-blue-500 hover:text-blue-700" href="#">More Info</a>
+                <a class="text-blue-500 hover:text-blue-700" href="#">Demo</a>
+            </div>
+            </div>
+        </div>
 </div>
-
+</div>
     )
 }
